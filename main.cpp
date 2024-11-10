c75383b8f718cc58cabcba70cb1da70e57709021
ab-flies <abtb2@alu.ua.es>
2024-11-10 21:21:16 +0100

    Started project

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <glib-2.0/glib.h>
+
+using namespace std;
+
+struct User {
+
+};
+
+int main() {
+    g_message("Hola");
+    g_message("Hola");
+    cout << "Hello World!" << endl;
+    // TODO: Add your code here
+
+    return 0;
+}
